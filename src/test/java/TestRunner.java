61ea75c2341ce28ced8e3aee5212b95d3b47f41e
thepulkitgupta <thepulkitgupta@gmail.com>
2021-05-02 20:15:20 +0530

    just some reordering

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -1,8 +1,10 @@
+import DTO.Members;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
         features = "src/test/java",
+        glue = {"DTO","Features","Steps"},
         tags = "@this",
         plugin = "html:target/Output.html"
 )
